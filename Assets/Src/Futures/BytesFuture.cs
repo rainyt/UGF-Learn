b1ec0d21aa9681a13f3ece1c871f084b1d92bd37
Rainy <939029835@qq.com>
2026-04-11 09:27:08 +0800

    新增对.bytes的数据加载支持

--- Assets/Src/Futures/BytesFuture.cs
+++ Assets/Src/Futures/BytesFuture.cs
@@ -1,9 +1,10 @@
 namespace Futures
 {
+    using System;
     using Unity.VisualScripting;
     using UnityEngine;
     using UnityGameFramework.Runtime;
-    
+
     /**
      * BytesFuture类表示一个异步加载二进制数据的Future，可以包含成功的结果数据或者失败的错误信息。
      */
@@ -17,6 +18,40 @@ namespace Futures
         public override void Post()
         {
             base.Post();
+            // 需要检查一次YooPackage是否存在
+            foreach (var package in this.Manager.Packages)
+            {
+                try
+                {
+                    var isExist = package.Value.CheckLocationValid(this.requestData);
+                    if (isExist)
+                    {
+                        Debug.Log($"[YooAsset] Load image asset from package: {package.Key}, asset name: {this.requestData}");
+                        var handle = package.Value.LoadAssetAsync<TextAsset>(this.requestData);
+                        handle.Completed += (result) =>
+                        {
+                            if (result.Status == YooAsset.EOperationStatus.Succeed)
+                            {
+                                TextAsset asset = result.AssetObject as TextAsset;
+                                this.CompleteValue(asset.bytes);
+                            }
+                            else
+                            {
+                                this.ErrorValue($"加载字节资源{this.requestData}失败，错误信息：{result.ToString()}");
+                            }
+                        };
+                        return;
+                    }
+                    else
+                    {
+                        Debug.Log($"[YooAsset] CheckLocationValid: {package.Key}, asset name: {this.requestData}, isExist: {isExist}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError($"[YooAsset] CheckLocationValid: {package.Key}, asset name: {this.requestData}, error: {e.Message}");
+                }
+            }
             this.resource.LoadBinary(this.requestData, new GameFramework.Resource.LoadBinaryCallbacks(
                 (assetName, bytes, duration, userData) =>
                 {
