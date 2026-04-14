aa7e800628b067f69d05c18149ed998da1fe29d0
Rainy <939029835@qq.com>
2026-04-14 11:02:08 +0800

    新增飞机位移逻辑

--- Assets/Src/Displays/Hero.cs
+++ Assets/Src/Displays/Hero.cs
@@ -7,6 +7,9 @@ namespace Displays
     public class Hero : EntityLogic
     {
 
+        private float _x = 0;
+        private float _y = 0;
+
         /// <summary>
         /// 移动到指定位置。
         /// </summary>
@@ -14,7 +17,15 @@ namespace Displays
         /// <param name="y"></param>
         public void MoveTo(float x, float y)
         {
-            transform.position = new Vector3(x, y, 0);
+            _x = x;
+            _y = y;
+        }
+
+        protected override void OnUpdate(float elapseSeconds, float realElapseSeconds)
+        {
+            base.OnUpdate(elapseSeconds, realElapseSeconds);
+            Vector3 current = transform.position;
+            transform.position = new Vector3(current.x + (_x - current.x) * 0.5f, current.y + (_y - current.y) * 0.5f, 0);
         }
     }
 }
