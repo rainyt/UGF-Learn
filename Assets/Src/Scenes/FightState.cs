502d877c37470212619ecac2f65cf2d09129222c
Rainy <939029835@qq.com>
2026-04-19 15:46:25 +0800

    新增分数居中文本

--- Assets/Src/Scenes/FightState.cs
+++ Assets/Src/Scenes/FightState.cs
@@ -2,8 +2,15 @@ using FrameworkCore.UI;
 
 namespace Scenes
 {
+    /// <summary>
+    /// 战斗中的状态栏支持
+    /// </summary>
     public class FightState : FairyUIFormLogic
     {
 
+        protected override void OnOpen(object userData)
+        {
+            base.OnOpen(userData);
+        }
     }
 }
